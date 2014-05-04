ec82e8093a65f79c811685036c2fec6c5668b5c7
Cristian Capdevila <ccapdevi@utk.edu>
2014-05-03 21:56:09 -0400

    big restructure, things were getting unreadable

--- toyfs.cpp
+++ toyfs.cpp
@@ -1,128 +1,123 @@
 #include "toyfs.hpp"
 #include <cmath>
 #include <iostream>
+#include <string>
+#include <vector>
 
-#define ops_at_least(x) \
-    if((int)args.size() < x+1) {\
-        std::cerr << args[0] << ": missing operand" << std::endl;\
-        return;\
-    }
-#define ops_less_than(x) \
-    if((int)args.size() > x+1) {\
-        std::cerr << args[0] << ": too many operands" << std::endl;\
-        return;\
-    }
-#define ops_exactly(x)\
-    ops_at_least(x);\
-    ops_less_than(x);
+#define ops_at_least(x)                                         \
+  if (static_cast<int>(args.size()) < x+1) {                    \
+    std::cerr << args[0] << ": missing operand" << std::endl;   \
+    return;                                                     \
+  }
 
-ToyFS::DirEntry::DirEntry(const std::string              name,
-                          const std::weak_ptr<DirEntry>& parent)
-  : name(name), parent(parent) {}
+#define ops_less_than(x)                                        \
+  if (static_cast<int>(args.size()) > x+1) {                    \
+    std::cerr << args[0] << ": too many operands" << std::endl; \
+    return;                                                     \
+  }
 
-ToyFS::DirEntry::DirEntry(const std::string              name,
-                          const std::weak_ptr<DirEntry>& parent,
-                          const std::shared_ptr<Inode> & inode)
-  : name(name), parent(parent), inode(inode) {}
+#define ops_exactly(x)                          \
+  ops_at_least(x);                              \
+  ops_less_than(x);
 
-void ToyFS::init_disk(const std::string& filename)
-{
+
+ToyFS::ToyFS(const std::string& filename,
+             const uint         fs_size,
+             const uint         block_size)
+    : filename(filename),
+      fs_size(fs_size),
+      block_size(block_size),
+      num_blocks(std::ceil(fs_size / block_size)),
+      root_dir("root", std::make_shared<DirEntry>(root_dir)) {
+
+  init_disk(filename);
+  free_nodes.emplace_back(num_blocks, 0);
+}
+
+ToyFS::~ToyFS() {
+  disk_file.close();
+  remove(filename.c_str());
+}
+
+void ToyFS::init_disk(const std::string& filename) {
   const std::vector<char>zeroes(num_blocks, 0);
 
   disk_file.open(filename,
-                   std::fstream::in |
-                   std::fstream::out |
-                   std::fstream::binary |
-                   std::fstream::trunc);
+                 std::fstream::in |
+                 std::fstream::out |
+                 std::fstream::binary |
+                 std::fstream::trunc);
 
   for (uint i = 0; i < num_blocks; ++i) {
     disk_file.write(zeroes.data(), block_size);
   }
 }
 
-ToyFS::ToyFS(const std::string& filename,
-             const uint         fs_size,
-             const uint         block_size)
-  : filename(filename),
-  fs_size(fs_size),
-  block_size(block_size),
-  num_blocks(std::ceil(fs_size / block_size))
-{
-  init_disk(filename);
-  free_nodes.emplace_back(num_blocks, 0);
-}
-
-ToyFS::~ToyFS()
-{
-  disk_file.close();
-  remove(filename.c_str());
-}
-
 void ToyFS::open(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::read(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::write(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::seek(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::close(std::vector<std::string> args) {
-    ops_exactly(1);
+  ops_exactly(1);
 }
 
 void ToyFS::mkdir(std::vector<std::string> args) {
-    ops_at_least(1);
+  ops_at_least(1);
 }
 
 void ToyFS::rmdir(std::vector<std::string> args) {
-    ops_at_least(1);
+  ops_at_least(1);
 }
 
 void ToyFS::cd(std::vector<std::string> args) {
-    ops_exactly(1);
+  ops_exactly(1);
 }
 
 
 void ToyFS::link(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::unlink(std::vector<std::string> args) {
-    ops_exactly(1);
+  ops_exactly(1);
 }
 
 void ToyFS::stat(std::vector<std::string> args) {
-    ops_at_least(1);
+  ops_at_least(1);
 }
 
 void ToyFS::ls(std::vector<std::string> args) {
-    ops_exactly(0);
+  ops_exactly(0);
 }
 
 void ToyFS::cat(std::vector<std::string> args) {
-    ops_at_least(1);
+  ops_at_least(1);
 }
 
 void ToyFS::cp(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::tree(std::vector<std::string> args) {
-    ops_exactly(0);
+  ops_exactly(0);
 }
 
 void ToyFS::import(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
 
 void ToyFS::FS_export(std::vector<std::string> args) {
-    ops_exactly(2);
+  ops_exactly(2);
 }
--- toyfs.hpp
+++ toyfs.hpp
@@ -1,53 +1,17 @@
-#pragma once
-#include <sys/types.h>
-#include <memory>
-#include <array>
-#include <list>
-#include <vector>
-#include <string>
+#ifndef _TOYFS_H_
+#define _TOYFS_H_
+
 #include <fstream>
+#include <list>
+#include <string>
+#include <vector>
+#include "inode.hpp"
+#include "direntry.hpp"
+#include "freenode.hpp"
 
 enum Mode { r, w, rw };
 
-enum EntryType { file, dir };
-
 class ToyFS {
-  class Inode {
-    public:
-
-    uint size;
-    uint block_size;
-    std::array<uint, 50>d_blocks;
-    std::unique_ptr < std::array < std::array<uint, 50>, 50 >> i_blocks;
-  };
-
-  class DirEntry {
-    public:
-
-    EntryType type;
-    std::string name;
-    std::weak_ptr<DirEntry>parent;
-    std::shared_ptr<Inode>inode;
-    std::list < std::shared_ptr < DirEntry >> subdirs;
-    DirEntry(const std::string              name,
-             const std::weak_ptr<DirEntry>& parent);
-    DirEntry(const std::string              name,
-             const std::weak_ptr<DirEntry>& parent,
-             const std::shared_ptr<Inode> & inode);
-
-    // move creation out to toyfs
-  };
-
-  class FreeNode {
-    public:
-
-    uint num_blocks;
-    uint pos;
-    FreeNode(uint num_blocks, uint pos)
-      : num_blocks(num_blocks),
-      pos(pos) {}
-  };
-
   const std::string filename;
   std::fstream disk_file;
   const uint fs_size;
@@ -55,11 +19,12 @@ class ToyFS {
   const uint num_blocks;
 
   // DirEntry root;
+  DirEntry root_dir;
   std::list<FreeNode>free_nodes;
 
   void init_disk(const std::string& filename);
 
-  public:
+ public:
   ToyFS(const std::string& filename, const uint fs_size, const uint block_size);
   ~ToyFS();
   void open(std::vector<std::string> args);
@@ -80,3 +45,5 @@ class ToyFS {
   void import(std::vector<std::string> args);
   void FS_export(std::vector<std::string> args);
 };
+
+#endif /* _TOYFS_H_ */
